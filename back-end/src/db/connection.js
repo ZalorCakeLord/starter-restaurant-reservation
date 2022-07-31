91a935b95e95df538a74864e14304c6766500b34
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2022-07-30 19:51:01 -0500

    completely crashed remote

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,8 +1,5 @@
 const environment = process.env.NODE_ENV || "development";
 let config = require("../../knexfile")[environment];
-if(environment === "development") {
-    config = process.env.DATABASE_URL_DEVELOPMENT
-}
 const knex = require("knex")(config);
 
 module.exports = knex;
