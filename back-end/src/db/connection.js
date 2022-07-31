11105579f91107d960b1da91c24a1f9286b39c9e
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2022-07-30 20:00:05 -0500

    you WILL use the dev db

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,4 +1,5 @@
-const environment = process.env.NODE_ENV || "development";
+let environment = process.env.NODE_ENV || "development";
+if(process.env.DATABASE_URL_DEVELOPMENT) environment = "development";
 let config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 
