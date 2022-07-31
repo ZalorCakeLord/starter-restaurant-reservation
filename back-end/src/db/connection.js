fe5304f1d66b68e624631b1d77fbc7cb304d6783
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2022-07-30 19:43:22 -0500

    more attempts at fixing env problem

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,5 +1,8 @@
 const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
+if(environment === "development") {
+    config = process.env.DATABASE_URL_DEVELOPMENT
+}
 const knex = require("knex")(config);
 
 module.exports = knex;
